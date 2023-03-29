008fd14bbb9daa1b549e3b6296f7c06295c05e6c
andriibildii <andrii.bildii@gmail.com>
2023-03-29 23:13:01 +0300

    added responsive on the navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -26,7 +26,7 @@ export default function NavBar() {
     }, []);
 
     useEffect(() => {
-        if (screenSize && screenSize < 768) {
+        if (screenSize && screenSize < 1024) {
             setActiveMenu(false);
             setSmallScreen(true);
         } else {
@@ -36,82 +36,91 @@ export default function NavBar() {
     }, [screenSize]);
 
     return (
-        <nav
-            className={`h-200 border-b px-5 ${
+        <div
+            className={`h-200 ${smallScreen ? "border-b" : "bg-[#F5F5F5]"} ${
                 smallScreen && activeMenu ? "bg-black text-white" : ""
-            } text-sm md:text-base`}
+            } text-sm lg:text-base`}
         >
-            <div className="flex w-full flex-wrap justify-between py-7">
-                <div className="w-full md:flex">
-                    <div className="flex w-full items-center justify-between">
-                        <NavLink to="/">
-                            <h1 className="font-inter text-lg leading-[22px]">
-                                Logo Here
-                            </h1>
-                        </NavLink>
-                        {smallScreen && (
-                            <div>
-                                {!activeMenu ? (
-                                    <button
-                                        onClick={() =>
-                                            setActiveMenu(!activeMenu)
-                                        }
-                                        className="transition duration-300 ease-out"
-                                    >
-                                        <img src={MenuIcon} alt="menu" />
-                                    </button>
-                                ) : (
-                                    <button
-                                        onClick={() =>
-                                            setActiveMenu(!activeMenu)
-                                        }
-                                        className="transition duration-300 ease-out"
-                                    >
-                                        <img src={CloseIcon} alt="close menu" />
-                                    </button>
-                                )}
-                            </div>
+            <div className="flex flex-row flex-wrap justify-between pl-5 pr-[23px] pt-[29px] pb-[30px] xl:px-[100px] xl:py-[41px]">
+                <div className="w-full lg:flex">
+                    <div className="flex w-full flex-wrap items-center justify-between lg:flex-nowrap">
+                        <nav className="flex w-full justify-between">
+                            <NavLink to="/">
+                                <h1 className="font-inter text-lg leading-[22px] xl:text-2xl xl:font-extrabold xl:leading-[29.05px]">
+                                    Logo Here
+                                </h1>
+                            </NavLink>
+                            {smallScreen && (
+                                <div>
+                                    {!activeMenu ? (
+                                        <button
+                                            onClick={() =>
+                                                setActiveMenu(!activeMenu)
+                                            }
+                                            className="transition duration-300 ease-out"
+                                        >
+                                            <img src={MenuIcon} alt="menu" />
+                                        </button>
+                                    ) : (
+                                        <button
+                                            onClick={() =>
+                                                setActiveMenu(!activeMenu)
+                                            }
+                                            className="transition duration-300 ease-out"
+                                        >
+                                            <img
+                                                src={CloseIcon}
+                                                alt="close menu"
+                                            />
+                                        </button>
+                                    )}
+                                </div>
+                            )}
+                        </nav>
+
+                        {activeMenu && (
+                            <>
+                                <div
+                                    className={`flex flex-col gap-[30px] pt-4 max-lg:w-full lg:flex-row lg:items-center lg:gap-[47px] lg:pt-0 xl:text-base xl:font-normal xl:leading-[18.75px]`}
+                                >
+                                    <NavLink to="/">Home</NavLink>
+                                    <NavLink to="#">Features</NavLink>
+                                    <NavLink to="#">Blog</NavLink>
+                                    <NavLink to="#">Shop</NavLink>
+                                    <NavLink to="#">About</NavLink>
+                                    <NavLink to="/contact">Contact</NavLink>
+
+                                    {/* After developing the rest of the pages, must be specified the path that leads to these pages */}
+                                </div>
+                                <div className="my-[30px] flex justify-center gap-9 max-lg:w-full lg:my-0 lg:pl-[60px]">
+                                    <div className="flex h-[33px] w-[31px] items-center justify-center rounded-full border lg:border-black">
+                                        <img
+                                            src={
+                                                smallScreen
+                                                    ? UserIcon
+                                                    : UserIconBl
+                                            }
+                                            alt="user"
+                                            className="w-4"
+                                        />
+                                    </div>
+                                    <div className="flex h-[31px] w-[31px] items-center justify-center rounded-full border lg:border-black">
+                                        <img
+                                            src={
+                                                smallScreen
+                                                    ? ShoppingIcon
+                                                    : ShoppingIconBl
+                                            }
+                                            alt="shopping"
+                                            className="w-4"
+                                        />
+                                    </div>
+                                </div>
+                            </>
                         )}
                     </div>
-
-                    {activeMenu && (
-                        <div
-                            className={`${
-                                activeMenu && smallScreen ? "" : ""
-                            } flex flex-col gap-8 pt-4 ease-out md:flex-row md:items-center md:pt-0 md:transition-transform md:duration-1000`}
-                        >
-                            {/* After developing the rest of the pages, must be specified the path that leads to these pages */}
-                            <NavLink to="/">Home</NavLink>
-                            <NavLink to="#">Features</NavLink>
-                            <NavLink to="#">Blog</NavLink>
-                            <NavLink to="#">Shop</NavLink>
-                            <NavLink to="#">About</NavLink>
-                            <NavLink to="/contact">Contact</NavLink>
-                            <div className="my-[30px] flex justify-center gap-9 md:my-0">
-                                <div className="flex h-[33px] w-[33px] items-center justify-center rounded-full border md:border-black">
-                                    <img
-                                        src={
-                                            smallScreen ? UserIcon : UserIconBl
-                                        }
-                                        alt="user"
-                                        className=""
-                                    />
-                                </div>
-                                <div className=" flex h-[33px] w-[33px] items-center justify-center rounded-full border md:border-black">
-                                    <img
-                                        src={
-                                            smallScreen
-                                                ? ShoppingIcon
-                                                : ShoppingIconBl
-                                        }
-                                        alt="shopping"
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                    )}
                 </div>
             </div>
-        </nav>
+        </div>
     );
 }
